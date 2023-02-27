3e86221c2d192c8ab973852b8f4b2a00ebd332a9
calvin-projet <66921805+calvin-projet@users.noreply.github.com>
2023-02-27 06:13:45 +0100

    commit site web

---
+++ model.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+
+def prepare_data(data_path):
+    # Load the anime data from CSV file
+    df = pd.read_csv(data_path)
+
+    # Perform data cleaning, feature extraction, etc.
+    # For example, you might drop irrelevant columns, fill in missing values,
+    # transform categorical variables into numerical features, and so on.
+    
+    # ...
+
+    # Return the cleaned and transformed data
+    return X, y
+
+def predict_rating(anime_title, anime_genre, anime_description, anime_type, anime_producer, anime_studio):
+    # Load the trained model from disk (you might want to store it as a file)
+    model = load_model('data/model_xgb.pkl')
+
+    # Prepare the input data for prediction
+    X = pd.DataFrame({
+        'title': [anime_title],
+        'genre': [anime_genre],
+        'description': [anime_description],
+        'type': [anime_type],
+        'producer': [anime_producer],
+        'studio': [anime_studio]
+    })
+
+    # Make a rating prediction using the trained model
+    rating = model.predict(X)
+
+    # Return the predicted rating
+    return rating
