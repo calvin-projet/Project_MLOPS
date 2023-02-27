613b739093718a992bcabe692c994f708ae0c23a
calvin-projet <66921805+calvin-projet@users.noreply.github.com>
2023-02-27 08:11:41 +0100

    update site web

--- app.py
+++ app.py
@@ -1,44 +1,81 @@
 from flask import Flask, request, jsonify, send_file, render_template
-from model import predict_rating
 import os
 import pandas as pd
+import pickle
 
 app = Flask(__name__)
+model = pickle.load(open('data/model_xgb.pkl', 'rb'))
 
-static_dir = '/static'
-app.static_folder = static_dir
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('page.html')
-
-@app.route('/predict', methods=['POST'])
-def predict_rating():
-    if request.method == 'POST':
-        # Get the form data
-        title = request.form['title']
-        genre = request.form.getlist('genre')
-        description = request.form['description']
-        anime_type = request.form['type']
-        producer = request.form['producer']
-        studio = request.form.getlist('studio')
-        
-        # Preprocess the input data
-        input_df = pd.DataFrame({'title': [title],
-                                 'genre': [','.join(genre)],
-                                 'description': [description],
-                                 'type': [anime_type],
-                                 'producer': [producer],
-                                 'studio': [','.join(studio)]})
-        
-        # Make the prediction
-        # rating = model.predict(input_df)[0]
-        rating = 999
-        
-        # Render the result template
-        return render_template('result.html', title=title, genre=genre, description=description, type=anime_type, producer=producer, studio=studio, rating=rating)
-    else:
+    if request.method=='GET':
         return render_template('page.html')
+    
+    elif request.method == 'POST':
+        df_anime = pd.read_csv("data/Anime_data.csv")
+        df_anime = df_anime[:10000]
+        df_anime = df_anime.dropna(subset=["Rating"])
+        
+        df_anime = df_anime.drop('Title', axis=1)
+        df_anime = df_anime.drop('Aired', axis=1)
+        df_anime = df_anime.drop('Anime_id', axis=1)
+        df_anime = df_anime.drop('Link', axis=1)
+        df_anime = df_anime.drop('Source', axis=1)
+        df_anime = df_anime.drop('Episodes', axis=1)
+        df_anime = df_anime.drop('Popularity', axis=1)
+        df_anime = df_anime.drop('ScoredBy', axis=1)
+        df_anime = df_anime.drop('Members', axis=1)
+        df_anime = df_anime.drop('Synopsis', axis=1)
+        df_anime = df_anime.drop(['Rating'], axis=1)
+        
+        Genre = request.form['genre']
+        Type = request.form['type']
+        Producer = request.form['producer']
+        Studio = request.form['studio']
+        
+        df_anime.loc[0] = ({'Genre': str(Genre), 'Type': str(Type), 'Producer': str(Producer), 'Studio': str(Studio)})
+
+        
+        df_anime['Genre'] = df_anime['Genre'].fillna("['unknown']")
+        df_anime['Producer'] = df_anime['Producer'].fillna("['unknown']")
+        df_anime['Studio'] = df_anime['Studio'].fillna("['unknown']")
+        df_anime['Type'] = df_anime['Type'].fillna("['unknown']")
+        
+        df_anime['Genre'] = df_anime['Genre'].str.replace('[', '')
+        df_anime['Genre'] = df_anime['Genre'].str.replace(']', '')
+        df_anime['Genre'] = df_anime['Genre'].str.replace("'", '')
+        
+        df_anime['Producer'] = df_anime['Producer'].str.replace('[', '')
+        df_anime['Producer'] = df_anime['Producer'].str.replace(']', '')
+        df_anime['Producer'] = df_anime['Producer'].str.replace("'", '')
+        
+        df_anime['Studio'] = df_anime['Studio'].str.replace('[', '')
+        df_anime['Studio'] = df_anime['Studio'].str.replace(']', '')
+        df_anime['Studio'] = df_anime['Studio'].str.replace("'", '')
+        
+        df_anime['Type'] = df_anime['Type'].str.replace('[', '')
+        df_anime['Type'] = df_anime['Type'].str.replace(']', '')
+        df_anime['Type'] = df_anime['Type'].str.replace("'", '')
+        
+        dummies = df_anime["Studio"].str.get_dummies(', ').add_prefix('studio_')
+        df_anime = pd.concat([df_anime, dummies], axis=1)
+        df_anime = df_anime.drop('Studio', axis=1)
+        
+        dummies = df_anime["Type"].str.get_dummies(', ').add_prefix('type_')
+        df_anime = pd.concat([df_anime, dummies], axis=1)
+        df_anime = df_anime.drop('Type', axis=1)
+        
+        dummies = df_anime["Producer"].str.get_dummies(', ').add_prefix('producer_')
+        df_anime = pd.concat([df_anime, dummies], axis=1)
+        df_anime = df_anime.drop('Producer', axis=1)
+        
+        dummies = df_anime["Genre"].str.get_dummies(', ').add_prefix('genre_')
+        df_anime = pd.concat([df_anime, dummies], axis=1)
+        df_anime = df_anime.drop('Genre', axis=1)
+        
+        Prediction = model.predict(df_anime.iloc[[0]])
+        
+        return render_template('predict-rating.html', genre=Genre, type=Type, producer=Producer, studio=Studio, prediction=Prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8090)
+    app.run(debug=True, port=8085)
